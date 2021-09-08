1227493414d534a8267b8da8f25e79ceefc8cd5f
steeltx <realsteel.gnu@gmail.com>
2021-09-08 16:53:08 -0500

    Usando query methods para producto

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
 
@@ -11,6 +12,14 @@ public class ProductoRepository {
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado( cantidad, true);
     }
 
 }
