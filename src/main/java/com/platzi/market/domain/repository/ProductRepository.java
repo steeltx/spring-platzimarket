4164ea185145dde993e1b335b0a6b3689bfe486c
steeltx <realsteel.gnu@gmail.com>
2021-09-08 18:32:26 -0500

    Iniciando con MapStruct y Data Mapper

---
+++ src/main/java/com/platzi/market/domain/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.platzi.market.domain.repository;
+
+import com.platzi.market.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    List<Product> gellAll();
+    Optional<List<Product>> getByCategoryId(int category);
+    Optional<List<Product>> getScarseProducts(int quantity);
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+
+}
