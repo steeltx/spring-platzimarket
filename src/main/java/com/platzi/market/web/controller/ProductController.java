ce1bccdb00066c8afb1e9ae13adb2682d654599e
steeltx <realsteel.gnu@gmail.com>
2021-09-10 17:15:00 -0500

    RestController para Producto

---
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping()
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId);
+    }
+
+    @GetMapping("/category/{id}")
+    public Optional<List<Product>> getByCategory (@PathVariable("id") int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+
+    @PostMapping
+    public Product save(@RequestBody Product product){
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") int productId){
+        return productService.delete(productId);
+    }
+}
